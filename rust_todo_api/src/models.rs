87023eafbead680b61102a9a84b3ea9fa3cabd5b
Uday-Devnur <udayd@dheyo.ai>
2026-01-19 00:37:56 +0530

    Add CreateTodoReq and TodoResponse models to support todo creation and response structure

--- rust_todo_api/src/models.rs
+++ rust_todo_api/src/models.rs
@@ -1,4 +1,6 @@
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct RegisterReq {
@@ -12,7 +14,20 @@ pub struct LoginReq {
     pub password: String,
 }
 
+#[derive(Deserialize)]
+pub struct CreateTodoReq {
+    pub title: String,
+}
+
 #[derive(Serialize)]
 pub struct MessageResponse {
     pub message: String,
+}
+
+#[derive(Serialize)]
+pub struct TodoResponse {
+    pub id: Uuid,
+    pub title: String,
+    pub completed: bool,
+    pub created_at: NaiveDateTime,
 }
